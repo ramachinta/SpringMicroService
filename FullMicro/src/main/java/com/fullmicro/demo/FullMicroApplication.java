e745fe44b901b4a13d5c22f081522dcb6fed2add
RAMACHINTA <RAMACHINTA@LAPTOP-CGFF8OEI.mshome.net>
2020-10-29 04:53:57 +0530

    Initial Commit

---
+++ FullMicro/src/main/java/com/fullmicro/demo/FullMicroApplication.java
@@ -0,0 +1,13 @@
+package com.fullmicro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullMicroApplication.class, args);
+	}
+
+}
