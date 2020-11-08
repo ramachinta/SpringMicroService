e2fb5c71972db66b87b71b2b78ac322f8e7ecf80
RAMACHINTA <RAMACHINTA@LAPTOP-CGFF8OEI.mshome.net>
2020-11-08 06:35:59 +0530

    Updated files

--- FullMicro/src/test/java/com/fullmicro/microservices/FullMicroApplicationTests.java
+++ FullMicro/src/test/java/com/fullmicro/microservices/FullMicroApplicationTests.java
@@ -1,12 +1,12 @@
 package com.fullmicro.microservices;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FullMicroApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
